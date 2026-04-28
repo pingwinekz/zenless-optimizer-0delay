9909ab2612d93585cad6e8a0337f075f9a3bded7
pingwinekZ <pingwinekzlibanu@gmail.com>
2026-04-28 16:19:12 +0200

    Add StarlightBilly meta

---
+++ libs/zzz/formula/src/meta/char/StarlightBilly/buffs.ts
@@ -0,0 +1,69 @@
+// WARNING: Generated file, do not modify
+export const buffs = {
+  core_critDmg: {
+    sheet: 'StarlightBilly',
+    name: 'core_critDmg',
+    tag: {
+      et: 'display',
+      qt: 'combat',
+      q: 'crit_dmg_',
+      sheet: 'StarlightBilly',
+      name: 'core_critDmg',
+    },
+  },
+  ability_dmg_: {
+    sheet: 'StarlightBilly',
+    name: 'ability_dmg_',
+    tag: {
+      et: 'display',
+      qt: 'combat',
+      q: 'common_dmg_',
+      sheet: 'StarlightBilly',
+      name: 'ability_dmg_',
+    },
+  },
+  m1_physResIgn: {
+    sheet: 'StarlightBilly',
+    name: 'm1_physResIgn',
+    tag: {
+      et: 'display',
+      qt: 'combat',
+      q: 'resIgn_',
+      sheet: 'StarlightBilly',
+      name: 'm1_physResIgn',
+    },
+  },
+  m2_dmg_: {
+    sheet: 'StarlightBilly',
+    name: 'm2_dmg_',
+    tag: {
+      et: 'display',
+      qt: 'combat',
+      q: 'common_dmg_',
+      sheet: 'StarlightBilly',
+      name: 'm2_dmg_',
+    },
+  },
+  m4_critDmg: {
+    sheet: 'StarlightBilly',
+    name: 'm4_critDmg',
+    tag: {
+      et: 'display',
+      qt: 'combat',
+      q: 'crit_dmg_',
+      sheet: 'StarlightBilly',
+      name: 'm4_critDmg',
+    },
+  },
+  m6_sheerDmg: {
+    sheet: 'StarlightBilly',
+    name: 'm6_sheerDmg',
+    tag: {
+      et: 'display',
+      qt: 'combat',
+      q: 'common_dmg_',
+      sheet: 'StarlightBilly',
+      name: 'm6_sheerDmg',
+    },
+  },
+} as const
